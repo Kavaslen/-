ced85cbb2ea55668df08c7210b368df9ea571af6
Dmitrii <kavaslen@yandex.ru>
2024-02-06 17:31:50 +0300

    mama

---
+++ characters.js
@@ -0,0 +1,147 @@
+var charactersData = {
+	group1: [
+		{ name: 'Роджер', image: 'img/', link: 'characters/roger.html' },
+		{ name: 'Шанкс', image: 'img/', link: 'characters/shansk.html' },
+		{ name: 'Бекман', image: 'img/', link: 'characters/beckman.html' },
+		{ name: 'Лаки', image: 'img/', link: 'characters/laki.html' },
+		{ name: 'Ясопп', image: 'img/', link: 'characters/yassop.html' },
+		{ name: 'Макино', image: 'img/', link: 'characters/makino.html' },
+		{ name: 'Вул', image: 'img/', link: 'characters/vul.html' },
+		{ name: 'Хигума', image: 'img/', link: 'characters/higuma.html' },
+		{ name: 'Коби', image: 'img/', link: 'characters/kobi.html' },
+		{ name: 'Альвида', image: 'img/', link: 'characters/alvida.html' },
+		{ name: 'Хельмеппо', image: 'img/', link: 'characters/helmeppo.html' },
+		{ name: 'Moрнан', image: 'img/', link: 'characters/.html' },
+		{ name: 'Косиро', image: 'img/', link: 'characters/.html' },
+		{ name: 'Куина', image: 'img/', link: 'characters/.html' },
+		{ name: 'Домо-кун', image: 'img/', link: 'characters/.html' },
+		{ name: 'Кабаджи', image: 'img/', link: 'characters/.html' },
+		{ name: 'Багги', image: 'img/', link: 'characters/.html' },
+		{ name: 'Ричи', image: 'img/', link: 'characters/.html' },
+		{ name: 'Моджи', image: 'img/', link: 'characters/.html' },
+		{ name: 'Будл', image: 'img/', link: 'characters/.html' },
+		{ name: 'Шушу', image: 'img/', link: 'characters/.html' },
+		{ name: 'Рейли', image: 'img/', link: 'characters/.html' },
+		{ name: 'Гаймон', image: 'img/', link: 'characters/.html' },
+		{ name: 'Куро', image: 'img/', link: 'characters/.html' },
+		{ name: 'Кая', image: 'img/', link: 'characters/.html' },
+		{ name: 'Пииман', image: 'img/', link: 'characters/.html' },
+		{ name: 'Таманэги', image: 'img/', link: 'characters/.html' },
+		{ name: 'Нинжин', image: 'img/', link: 'characters/.html' },
+		{ name: 'Усопп', image: 'img/', link: 'characters/.html' },
+		{ name: 'Мерри', image: 'img/', link: 'characters/.html' },
+		{ name: 'Джанго', image: 'img/', link: 'characters/.html' },
+		{ name: 'Бути', image: 'img/', link: 'characters/.html' },
+		{ name: 'Сям', image: 'img/', link: 'characters/.html' },
+		{ name: 'Банкина', image: 'img/', link: 'characters/.html' },
+		{ name: 'Ёсаку', image: 'img/', link: 'characters/.html' },
+		{ name: 'Джонни', image: 'img/', link: 'characters/.html' },
+		{ name: 'Зефф', image: 'img/', link: 'characters/.html' },
+		{ name: 'Фулботи', image: 'img/', link: 'characters/.html' },
+		{ name: 'Патти', image: 'img/', link: 'characters/.html' },
+		{ name: 'Гин', image: 'img/', link: 'characters/.html' },
+		{ name: 'Пандамен', image: 'img/', link: 'characters/.html' },
+		{ name: 'Криг', image: 'img/', link: 'characters/.html' },
+		{ name: 'Михоук', image: 'img/', link: 'characters/.html' },
+		{ name: 'Перл', image: 'img/', link: 'characters/.html' },
+		{ name: 'Чу', image: 'img/', link: 'characters/.html' },
+		{ name: 'Нэдзуми', image: 'img/', link: 'characters/.html' },
+		{ name: 'Хаттян', image: 'img/', link: 'characters/.html' },
+		{ name: 'Курооби', image: 'img/', link: 'characters/.html' },
+		{ name: 'Арлонг', image: 'img/', link: 'characters/.html' },
+		{ name: 'Ноджико', image: 'img/', link: 'characters/.html' },
+		{ name: 'Ганзо', image: 'img/', link: 'characters/.html' },
+		{ name: 'Мому', image: 'img/', link: 'characters/.html' },
+		{ name: 'Белл-мера', image: 'img/', link: 'characters/.html' },
+		{ name: 'Нако', image: 'img/', link: 'characters/.html' },
+		{ name: 'Богарт', image: 'img/', link: 'characters/.html' },
+		{ name: 'Гарп', image: 'img/', link: 'characters/.html' },
+		{ name: 'Ташиги', image: 'img/', link: 'characters/.html' },
+		{ name: 'Иппон-Мацу', image: 'img/', link: 'characters/.html' },
+		{ name: 'Смокер', image: 'img/', link: 'characters/.html' },
+		{ name: 'Драгон', image: 'img/', link: 'characters/.html' },
+		{ name: 'Крокус', image: 'img/', link: 'characters/.html' },
+		{ name: 'Лабун', image: 'img/', link: 'characters/.html' },
+		{ name: 'Мистер 9', image: 'img/', link: 'characters/.html' },
+		{ name: 'Виви', image: 'img/', link: 'characters/.html' },
+		{ name: 'Фарайдей', image: 'img/', link: 'characters/.html' },
+		{ name: 'Мистер 13', image: 'img/', link: 'characters/.html' },
+		{ name: 'Игарам', image: 'img/', link: 'characters/.html' },
+		{ name: 'Мандей', image: 'img/', link: 'characters/.html' },
+		{ name: 'Симиджу', image: 'img/', link: 'characters/.html' },
+		{ name: 'Кару', image: 'img/', link: 'characters/.html' },
+		{ name: 'Мистер 5', image: 'img/', link: 'characters/.html' },
+		{ name: 'Валентайн', image: 'img/', link: 'characters/.html' },
+		{ name: 'Дорри', image: 'img/', link: 'characters/.html' },
+		{ name: 'Броги', image: 'img/', link: 'characters/.html' },
+		{ name: 'Голдевник', image: 'img/', link: 'characters/.html' },
+		{ name: 'Галдино', image: 'img/', link: 'characters/.html' },
+		{ name: 'Крокодайл', image: 'img/', link: 'characters/.html' },
+		{ name: 'Бонтян', image: 'img/', link: 'characters/.html' },
+		{ name: 'Вапол', image: 'img/', link: 'characters/.html' },
+		{ name: 'Чесс', image: 'img/', link: 'characters/.html' },
+		{ name: 'Куромаримо', image: 'img/', link: 'characters/.html' },
+		{ name: 'Далтон', image: 'img/', link: 'characters/.html' },
+		{ name: 'Куреха', image: 'img/', link: 'characters/.html' },
+		{ name: 'Робсон', image: 'img/', link: 'characters/.html' },
+		{ name: 'Хилюлюк', image: 'img/', link: 'characters/.html' },
+		{ name: 'Кобра', image: 'img/', link: 'characters/.html' },
+		{ name: 'Эйс', image: 'img/', link: 'characters/.html' },
+		{ name: 'Пелл', image: 'img/', link: 'characters/.html' },
+		{ name: 'Чака', image: 'img/', link: 'characters/.html' },
+		{ name: 'Даблфингер', image: 'img/', link: 'characters/.html' },
+		{ name: 'Кристмас', image: 'img/', link: 'characters/.html' },
+		{ name: 'Лассу', image: 'img/', link: 'characters/.html' },
+		{ name: 'Мистер 4', image: 'img/', link: 'characters/.html' },
+		{ name: 'Бонс', image: 'img/', link: 'characters/.html' },
+		{ name: 'Мацуге', image: 'img/', link: 'characters/.html' },
+		{ name: 'Коза', image: 'img/', link: 'characters/.html' },
+		{ name: 'Тото', image: 'img/', link: 'characters/.html' },
+		{ name: 'Хина', image: 'img/', link: 'characters/.html' },
+		{ name: 'Хасами', image: 'img/', link: 'characters/.html' },
+		{ name: 'Кейми', image: 'img/', link: 'characters/.html' },
+		{ name: 'Паппг', image: 'img/', link: 'characters/.html' },
+	],
+	group2: [
+		{ name: 'Чоппер', image: 'img/chopper.jpg', link: 'characters/chopper.html' },
+		// Добавьте других персонажей для Группы 2
+	],
+	// Добавьте другие группы и их персонажей при необходимости
+	group3: [
+		{ name: 'Чоппер', image: 'img/chopper.jpg', link: 'characters/chopper.html' },
+		// Добавьте других персонажей для Группы 2
+	],
+	group4: [
+		{ name: 'Чоппер', image: 'img/chopper.jpg', link: 'characters/chopper.html' },
+		// Добавьте других персонажей для Группы 2
+	],
+	group5: [
+		{ name: 'Чоппер', image: 'img/chopper.jpg', link: 'characters/chopper.html' },
+		// Добавьте других персонажей для Группы 2
+	],
+};
+
+// Функция для изменения отображаемой группы персонажей
+function changeGroup(group) {
+	var charactersContainer = document.getElementById('charactersContainer');
+	charactersContainer.innerHTML = ''; // Очищаем контейнер перед добавлением новых карточек
+
+	// Перебираем персонажей выбранной группы и формируем HTML
+	charactersData[group].forEach(function(character) {
+		var cardHtml = `
+				<div class="col mb-4">
+						<div class="card text-bg-dark shadow-sm bg-body-tertiary position-relative overflow-hidden">
+								<img src="${character.image}" class="img-fluid object-fit-cover" style="height: 300px;" alt="...">
+								<div class="card-img-overlay d-flex align-items-end">
+										<a href="${character.link}"><p class="card-title card-black stretched-link text-danger-hover">${character.name}</p></a>
+								</div>
+						</div>
+				</div>
+		`;
+
+	charactersContainer.innerHTML += cardHtml; // Добавляем каждую карточку в контейнер
+});
+}
+
+// Изначально отображаем первую группу при загрузке страницы
+changeGroup('group1');
